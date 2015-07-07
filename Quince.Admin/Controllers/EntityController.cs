7582d94a95ba2e2ead410e7caf9e90eb8094d833
heightpi <mihai.popescu.cedacri@gmail.com>
2015-07-08 00:28:31 +0300

    Third commit

---
+++ Quince.Admin/Controllers/EntityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quince.Admin.Controllers
+{
+    public class EntityController : Controller
+    {
+        // GET: Entity
+        public ActionResult Details(long id)
+        {
+            return View();
+        }
+    }
+}
